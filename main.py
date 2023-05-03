694e77cb89ee27f01de622b3a268eba4a6512470
매코 <53283147+seokjw0727@users.noreply.github.com>
2023-05-03 20:17:52 +0900

    슬래시 명령어로 변환 및 코드 가독성 향상. 주석 구체화 예정

--- main.py
+++ main.py
@@ -33,14 +33,6 @@ async def on_ready():
         print(f"에러: {e}")
 
 
-@bot.tree.command(name="안녕")
-async def hello(interaction: discord.Interaction):
-    await interaction.response.send_message("반가워요!")  
-
-@bot.tree.command(name="말해")
-@app_commands.describe(말씀="말할 내용")
-async def say(interaction: discord.Interaction, 말씀: str):
-    await interaction.response.send_message(f"{interaction.user.mention} 님의 말씀: {말씀}!")
 
 
 
@@ -57,7 +49,7 @@ async def on_message(message):
 
 
 
-@bot.tree.command(name="상태")
+@bot.tree.command(name="상태") # '상태' 명령어
 async def status(interaction: discord.Interaction):
     if bot.latency * 1000 >= 210:
         embed_red = discord.Embed(title='상태', description=f'Ping: {bot.latency * 1000}ms', color=0xff0000)
@@ -66,23 +58,18 @@ async def status(interaction: discord.Interaction):
         embed_green = discord.Embed(title='상태', description=f'Ping: {bot.latency * 1000}ms', color=0x00ff00)
         await interaction.response.send_message(embed=embed_green)
 
-
-# @bot.command() # '상태' 명령어, 봇의 상태를 embed로 보여줌.
-# async def 상태(ctx):
-#     # 만약 봇의 핑이 200 이상이라면 embed의 색을 빨강색으로 합니다.
-#     if bot.latency * 1000 >= 200:
-#         embed_red = discord.Embed(title='상태', description=f'Ping: {bot.latency * 1000}ms', color=0xff0000)
-#         await ctx.send(embed=embed_red)
-#     # 만약 봇의 핑이 200 미만이라면 embed의 색을 초록색으로 합니다.
-#     else:
-#         embed_green = discord.Embed(title='상태', description=f'Ping: {bot.latency * 1000}ms', color=0x00ff00)
-#         await ctx.send(embed=embed_green) # embed를 보냅니다.
+@status.error
+async def status_error(ctx, error):
+    if isinstance(error, commands.MissingPermissions):
+        embed = discord.Embed(title='거부', description='당신은 관리자 권한이 없습니다.', color=0xff0000)
+        await ctx.send(embed=embed)
+    else:
+        embed = discord.Embed(title='오류', description='알 수 없는 오류가 발생했습니다.', color=0xff0000)
+        await ctx.send(embed=embed)
 
 
 
-
-
-@bot.tree.command(name="청소")
+@bot.tree.command(name="청소") # '청소' 명령어
 @commands.has_permissions(administrator=True)
 @app_commands.describe(개수="청소할 메시지의 개수")
 async def clear_chat(interaction: discord.Interaction, amount: int):
@@ -111,55 +98,59 @@ async def clear_chat_error(ctx, error):
 
 
 
-# @bot.command() # '청소' 명령어, 메시지를 청소함.
-# @commands.has_permissions(administrator=True)
-# async def 청소(ctx, amount: int):
-#     if amount <= 0 or amount is None:
-#         embed = discord.Embed(title='오류', description='1 이상의 자연수를 입력해주세요.', color=0xff0000)
-#         await ctx.send(embed=embed)
-#     else:
-#         await ctx.channel.purge(limit=amount)
-#         embed = discord.Embed(title='청소 완료', description=f'{amount}개의 메시지를 삭제했습니다.', color=0x00ff00)
-#         await ctx.send(embed=embed)
-# @청소.error
-# async def 청소_error(ctx, error):
-#     if isinstance(error, commands.MissingPermissions):
-#         embed = discord.Embed(title='오류', description='당신은 관리자 권한이 없습니다.', color=0xff0000)
-#         await ctx.send(embed=embed)
-
-
-
-@bot.command() # '경고' 명령어, 유저에게 경고를 줌. (추가 기능 필요)
+@bot.tree.command(name="경고") # '경고' 명령어
 @commands.has_permissions(administrator=True)
-async def 경고(ctx, member: discord.Member, *, reason=None):
+@app_commands.describe(유저="경고를 줄 유저", 사유="경고 사유")
+async def warn(interaction: discord.Interaction, 유저: discord.Member, 사유: str):
     channel = bot.get_channel(1091003605240266875)
     if channel is None:
-        await ctx.send('경고 채널을 찾을 수 없습니다.')
+        await interaction.send('경고 채널을 찾을 수 없습니다.')
         return
-    warning_message = f"__{member}__ 님이 관리자에게 경고를 받았습니다.\n> 사유: {reason}"
+    warning_message = f"__{유저}__ 님이 관리자에게 경고를 받았습니다.\n> 사유: {사유}"
     await channel.send(warning_message)
-@경고.error
-async def 경고_error(ctx, error):
+    embed = discord.Embed(title='성공', description=f'{유저} 님에게 경고를 부여했습니다.', color=0x00ff00)
+    await interaction.send(embed=embed)
+
+@warn.error
+async def warn_error(ctx, error):
     if isinstance(error, commands.MissingPermissions):
         embed = discord.Embed(title='오류', description='당신은 관리자 권한이 없습니다.', color=0xff0000)
+        await ctx.send(embed=embed)
+    elif isinstance(error, commands.MissingRequiredArgument):
+        embed = discord.Embed(title='오류', description='경고를 줄 유저와 경고 사유를 입력해주세요.', color=0xff0000)
+        await ctx.send(embed=embed)
+    else:
+        embed = discord.Embed(title='오류', description='알 수 없는 오류가 발생했습니다.', color=0xff0000)
         await ctx.send(embed=embed)
 
 
 
-@bot.command() # '공지' 명령어, 특정 채널에 공지 embed를 전송함.
+
+@bot.tree.command(name="공지") # '공지' 명령어
+@app_commands.describe(내용="공지 내용")
 @commands.has_permissions(administrator=True)
-async def 공지(ctx, *, content):
+async def notice(interaction: discord.Interaction, 내용: str):
     channel = bot.get_channel(1094255511609802792)
     if channel is None:
-        await ctx.send('공지 채널을 찾을 수 없습니다.')
+        await interaction.send('공지 채널을 찾을 수 없습니다.')
         return
-    embed=discord.Embed(title="공지", description='> ' + content + ' \n \n ||@everyone||', color=0x00ff00)
+    embed=discord.Embed(title="공지", description='> ' + 내용 + ' \n \n ||@everyone||', color=0x00ff00)
     await channel.send(embed=embed)
-@공지.error
-async def 공지_error(ctx, error):
+    embed = discord.Embed(title='성공', description='공지를 보냈습니다.', color=0x00ff00)
+    await interaction.send(embed=embed)
+
+@notice.error
+async def notice_error(ctx, error):
     if isinstance(error, commands.MissingPermissions):
         embed = discord.Embed(title='오류', description='당신은 관리자 권한이 없습니다.', color=0xff0000)
         await ctx.send(embed=embed)
+    elif isinstance(error, commands.MissingRequiredArgument):
+        embed = discord.Embed(title='오류', description='공지 내용을 입력해주세요.', color=0xff0000)
+        await ctx.send(embed=embed)
+    else:
+        embed = discord.Embed(title='오류', description='알 수 없는 오류가 발생했습니다.', color=0xff0000)
+        await ctx.send(embed=embed)
+
         
         
 
