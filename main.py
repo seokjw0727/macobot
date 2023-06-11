31a1a7559d8f070c07306e99ea8c0d3bb7a64abd
매코 <53283147+seokjw0727@users.noreply.github.com>
2023-06-11 16:53:33 +0900

    '/마크' 명령어의 서버가 닫혔을 때, 응답하지 않던 오류 수정

--- main.py
+++ main.py
@@ -276,17 +276,16 @@ async def minecraft_server_check_test_version(interaction: discord.Interaction):
     try:
         logging_channel = bot.get_channel(1114564440092835990)
         await logging_channel.send(f"{interaction.user} 님이 '/minecraft_server_check' 명령어를 사용했습니다.")
-
-    except Exception as e:
         embed = discord.Embed(title='🔴오프라인🔴', description='서버가 오프라인이거나 오류가 발생했습니다.', color=0xff0000)
         await interaction.response.send_message(embed=embed)
 
-    server = JavaServer.lookup("124.60.247.163:25565")
-    status = server.status()
-    embed = discord.Embed(title='🟢온라인🟢', description='마크 서버의 상태를 확인합니다.', color=0x00ff00)
-    embed.add_field(name='서버 상태', value=f'{status.latency}ms', inline=False)
-    embed.add_field(name='현재 플레이어 수', value=f'{status.players.online}명', inline=False)
-    await interaction.response.send_message(embed=embed)
+    except status.latency > 0:
+        server = JavaServer.lookup("124.60.247.163:25565")
+        status = server.status()
+        embed = discord.Embed(title='🟢온라인🟢', description='마크 서버의 상태를 확인합니다.', color=0x00ff00)
+        embed.add_field(name='서버 상태', value=f'{status.latency}ms', inline=False)
+        embed.add_field(name='현재 플레이어 수', value=f'{status.players.online}명', inline=False)
+        await interaction.response.send_message(embed=embed)
 
 @minecraft_server_check_test_version.error
 async def minecraft_server_check_test_version_error(interaction: discord.Interaction, error):
