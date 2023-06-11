042638b1834494ce907fbe2d7642a74148080c22
매코 <53283147+seokjw0727@users.noreply.github.com>
2023-06-11 16:31:07 +0900

    '/마크' 명령어 수정중

--- main.py
+++ main.py
@@ -275,10 +275,10 @@ async def minecraft_server_check(interaction: discord.Interaction):
 async def minecraft_server_check_test_version(interaction: discord.Interaction):
     await logging_channel.send(f"{interaction.user} 님이 '/minecraft_server_check' 명령어를 사용했습니다.")
     logging_channel = bot.get_channel(1114564440092835990)
-    server = JavaServer.lookup("macosv.kro.kr")
+    server = JavaServer.lookup("124.60.247.163:25565")
     status = server.status()
     embed = discord.Embed(title='🟢온라인🟢', description='마크 서버의 상태를 확인합니다.', color=0x00ff00)
-    embed.add_field(name='서버 상태', value=f'{status.latency}ms', inline=False)
+    embed.add_field(name='서버 상태', value=f'{server.ping}ms', inline=False)
     embed.add_field(name='현재 플레이어 수', value=f'{status.players.online}명', inline=False)
     await interaction.response.send_message(embed=embed)
 
