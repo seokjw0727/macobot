4cff920b3957707aa2d22ce8faf5c31dc585fc04
매코 <53283147+seokjw0727@users.noreply.github.com>
2023-06-11 16:20:23 +0900

    '/마크' 명령어 수정중

--- main.py
+++ main.py
@@ -273,21 +273,16 @@ async def minecraft_server_check(interaction: discord.Interaction):
 @bot.tree.command(name= "minecraft_server_check", description= "매코 서버의 상태를 확인합니다.") # 'minecraft_server_check' 명령어 || '마크' 명령어를 수리하기 위해 만든 임시 명령어.
 @commands.has_permissions(administrator=True)
 async def minecraft_server_check_test_version(interaction: discord.Interaction):
-    logging_channel = bot.get_channel(1114564440092835990)
-    try:
-        server = JavaServer.lookup("124.60.247.163:25565")
-        status = server.status
-
-        embed = discord.Embed(title='🟢 **서버 열림** 🟢', description='현재 서버가 열려있습니다.', color=0x00ff00)
-        embed.add_field(name='서버 버전', value=f'`{status.version.name}`', inline=False)
-        embed.add_field(name='현재 접속자 수', value=f'`{status.players.online}`명', inline=False)
-        embed.add_field(name='최대 접속자 수', value=f'`{status.players.max}`명', inline=False)
-        embed.add_field(name='서버 핑', value=f'{round(status.latency)}ms', inline=False)
-        await interaction.response.send_message(embed=embed)
-    except ConnectionError:
-        embed = discord.Embed(title='🔴 **서버 닫힘** 🔴', description='현재 서버가 닫혀있습니다.', color=0xff0000)
-        await interaction.response.send_message(embed=embed)
     await logging_channel.send(f"{interaction.user} 님이 '/minecraft_server_check' 명령어를 사용했습니다.")
+    logging_channel = bot.get_channel(1114564440092835990)
+    server = JavaServer.lookup("macosv.kro.kr")
+    status = server.status()
+    embed = discord.Embed(title='🟢온라인🟢', description='마크 서버의 상태를 확인합니다.', color=0x00ff00)
+    embed.add_field(name='서버 상태', value=f'{status.ping}ms', inline=False)
+    embed.add_field(name='현재 플레이어 수', value=f'{status.players.online}명', inline=False)
+    await interaction.response.send_message(embed=embed)
+
+    
 
 @minecraft_server_check_test_version.error
 async def minecraft_server_check_test_version_error(interaction: discord.Interaction, error):
@@ -295,7 +290,7 @@ async def minecraft_server_check_test_version_error(interaction: discord.Interac
         embed = discord.Embed(title='❌거부❌', description='당신은 관리자 권한이 없습니다.', color=0xff0000)
         await interaction.response.send_message(embed=embed)
     else:
-        embed = discord.Embed(title='🛑오류🛑', description='알 수 없는 오류가 발생했습니다.', color=0xff0000)
+        embed = discord.Embed(title='🔴오프라인🔴', description='서버가 오프라인이거나 오류가 발생했습니다.', color=0xff0000)
         await interaction.response.send_message(embed=embed)
 
 
